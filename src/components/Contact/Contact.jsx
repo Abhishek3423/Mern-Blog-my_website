cd4203a37ab907dad973a6ed35bb5f759cd793f9
Abhishek3423 <abhishekkamati304@gmail.com>
2024-04-23 10:57:55 +0545

    fotter change

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:abhishekkamati99@gmail.com">abhishekkamati99@gmail.com</a>
+          <a href="mailto:abhishekkamati99@gmail.com">abhishekkamati/mail</a>
         </li>
         <li className={styles.link}>
           <img
